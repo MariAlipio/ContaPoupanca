53035ac224af8a3ed122a41b4fcba2d9c5ef2f74
MariAlipio <42498249+MariAlipio@users.noreply.github.com>
2018-10-06 09:52:40 -0300

    Add files via upload

---
+++ SaldoPoupanca/SaldoPoupanca/Poupanca.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaldoPoupanca
+{
+    class Poupanca
+    {
+        private static double taxadejuros;
+        private decimal saldopoupanca;
+
+        private int MESES = 12;
+        
+        public Poupanca() { }
+
+        public Poupanca(decimal saldopoupanca)
+        {
+            this.saldopoupanca = saldopoupanca;
+            taxadejuros = 0.0;
+        }
+
+        //Configurar saldo
+        public decimal Saldo
+        {
+            get
+            {
+                return saldopoupanca;
+            }
+            set
+            {
+                saldopoupanca = value;
+            }
+        }
+        //Pega ou configura a taxa de juros anual
+        public static double TaxadeJuros
+        {
+            get
+            {
+                return taxadejuros;
+            }
+            set
+            {
+                taxadejuros = value;
+            }
+        }
+        //Calcula o juros mensais da poupança acrescidos do saldo atual
+        public decimal CalcularJurosMensal()
+        {
+            decimal jurosMensal = ((saldopoupanca * (decimal)taxadejuros) / MESES);
+            decimal saldopoupador = jurosMensal + saldopoupanca;
+            return saldopoupador;
+        }
+        //Altera a taxa de juros da conta poupança
+        public static void AlterataxadeJuros(double Taxadejuros)
+        {
+            taxadejuros = Taxadejuros;
+        }
+    }
+}
---
+++ SaldoPoupanca/SaldoPoupanca/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SaldoPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Poupanca Carlos = new Poupanca(2000M);
+            Poupanca Guilherme = new Poupanca(3000M);
+                        
+            Console.WriteLine("Bem vindo");
+            Console.WriteLine("A taxa de juros atual é de 4%");
+            Poupanca.AlterataxadeJuros(0.04);
+            Console.WriteLine("Seu saldo é de:" + Carlos.CalcularJurosMensal());
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Bem vindo");
+            Console.WriteLine("A taxa de juros atual é de 4%");
+            Console.WriteLine("Seu saldo é de:" + Guilherme.CalcularJurosMensal());
+            Console.ReadKey();
+            Console.Clear();
+
+            //Alterar taxa para 5%
+            Console.WriteLine("Bem vindo");
+            Console.WriteLine("A taxa de juros atual é de 5%");
+            Poupanca.AlterataxadeJuros(0.05);
+            Console.WriteLine("Seu saldo é de:" + Carlos.CalcularJurosMensal());
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Bem vindo");
+            Console.WriteLine("A taxa de juros atual é de 5%");
+            Console.WriteLine("Seu saldo é de:" + Guilherme.CalcularJurosMensal());
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
